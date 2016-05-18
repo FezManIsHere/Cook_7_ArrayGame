91b1aeb7fc9baed733f87632eace21d1fb03dfcc
Carter Cook <alonzi.alonzo7@gmail.com>
2016-05-18 16:10:42 -0500

    Updates

--- src/arraygame/ArrayGame.java
+++ src/arraygame/ArrayGame.java
@@ -10,56 +10,51 @@ public class ArrayGame {
     public static void main(String[] args) {
         go();
     }
-
+    
+    static Player player;
+    static Enemy e1, e2, e3;
+    static String direction;
+    static boolean play = true;
+    static boolean enemyAlive = true;
+    static boolean enemyAlive1 = true;
+    static boolean chestAlive = true;
+    static boolean chestAlive1 = true;
+    static String playercoords = player.x + "," + player.y;
+    static String trap1;
+    static String trap2;
+    static String chest;
+    static String chest1;
+    static String enemy;
+    static String enemy1;
+    static Enemy[] enemies = {e1, e2, e3};
+    
     static void game() {
         System.out.println("\n\n\n\n");
         Scanner scan = new Scanner(System.in);
         Random rand1 = new Random();
-        char[][] map = new char[21][21];
-        int x = 10;
-        int y = 10;
-        int ex = rand1.nextInt(19) + 1;
-        int ey = rand1.nextInt(19) + 1;
-        int ex1 = rand1.nextInt(19) + 1;
-        int ey1 = rand1.nextInt(19) + 1;
-        int cx = rand1.nextInt(19) + 1;
-        int cy = rand1.nextInt(19) + 1;
-        int cx1 = rand1.nextInt(19) + 1;
-        int cy1 = rand1.nextInt(19) + 1;
-        int trapx = rand1.nextInt(19) + 1;
-        int trapy = rand1.nextInt(19) + 1;
-        int trap1x = rand1.nextInt(19) + 1;
-        int trap1y = rand1.nextInt(19) + 1;
-        String direction;
-        boolean play = true;
-        boolean enemyAlive = true;
-        boolean enemyAlive1 = true;
-        boolean chestAlive = true;
-        boolean chestAlive1 = true;
-        int score = 0;
-        String player = x + "," + y;
-        String trap1 = trapx + "," + trapy;
-        String trap2 = trap1x + "," + trap1y;
-        String chest = cx + "," + cy;
-        String chest1 = cx1 + "," + cy1;
-        String enemy = ex + "," + ey;
-        String enemy1 = ex1 + "," + ey1;
-        while (player.equals(trap1) || player.equals(trap2) || player.equals(chest) || player.equals(enemy) || trap1.equals(trap2) || trap1.equals(chest) || trap1.equals(enemy) || trap2.equals(enemy) || trap2.equals(chest) || enemy.equals(chest) || enemy1.equals(player) || enemy1.equals(trap1) || enemy1.equals(trap2) || enemy1.equals(chest) || enemy1.equals(chest1) || enemy1.equals(enemy) || chest1.equals(player) || chest1.equals(trap1) || chest1.equals(trap2) || chest1.equals(chest) || chest1.equals(enemy)) {
-            trapx = rand1.nextInt(19) + 1;
-            trapy = rand1.nextInt(19) + 1;
-            trap1x = rand1.nextInt(19) + 1;
-            trap1y = rand1.nextInt(19) + 1;
-            ex = rand1.nextInt(19) + 1;
-            ey = rand1.nextInt(19) + 1;
-            ex1 = rand1.nextInt(19) + 1;
-            ey1 = rand1.nextInt(19) + 1;
-            cx = rand1.nextInt(19) + 1;
-            cy = rand1.nextInt(19) + 1;
-            cx1 = rand1.nextInt(19) + 1;
-            cy1 = rand1.nextInt(19) + 1;
+        char[][] map = new char[41][41];
+        player = new Player("Hero",20,20,'@', 0);
+        
+        
+        for (int i = 0; i < enemies.length; i++) {
+            enemies[i] = new Enemy(true, rand1.nextInt(39) + 1, rand1.nextInt(39) + 1, 'P');
         }
+        
+        
+        int ex = rand1.nextInt(39) + 1;
+        int ey = rand1.nextInt(39) + 1;
+        int ex1 = rand1.nextInt(39) + 1;
+        int ey1 = rand1.nextInt(39) + 1;
+        int cx = rand1.nextInt(39) + 1;
+        int cy = rand1.nextInt(39) + 1;
+        int cx1 = rand1.nextInt(39) + 1;
+        int cy1 = rand1.nextInt(39) + 1;
+        int trapx = rand1.nextInt(39) + 1;
+        int trapy = rand1.nextInt(39) + 1;
+        int trap1x = rand1.nextInt(39) + 1;
+        int trap1y = rand1.nextInt(39) + 1;
         while (play) {
-            map[x][y] = '@';
+            map[player.x][player.y] = '@';
             map[trapx][trapy] = '*';
             map[trap1x][trap1y] = '*';
             if (enemyAlive) {
@@ -76,16 +71,16 @@ public class ArrayGame {
             }
             for (int i = 0; i <= map[0].length - 1; i++) {
                 for (int j = 0; j <= map[1].length - 1; j++) {
-                    if (i == 0 || j == 0 || i == 20 || j == 20) {
+                    if (i == 0 || j == 0 || i == 40 || j == 40) {
                         map[i][j] = '#';
                     }
-                    if (j < map[1].length - 1 /*&& (j < 10 + y || j > x - 10) && (i < 10 + x || i > y - 10)*/) {
+                    if (j < map[1].length - 1 /*&& (j < 10 + player.y || j > player.x - 10) && (i < 10 + player.x || i > player.y - 10)*/) {
                         if (map[i][j] != '@' && map[i][j] != '*' && map[i][j] != 'P' && map[i][j] != 'T' && map[i][j] != '#') {
                             System.out.print(". ");
                         } else {
                             System.out.print(map[i][j] + " ");
                         }
-                    } else if (map[i][j] != '@' && map[i][j] != '#'/*&& (j < 10 + y || j > x - 10) && (i < 10 + x || i > y - 10)*/) {
+                    } else if (map[i][j] != '@' && map[i][j] != '#'/*&& (j < 10 + player.y || j > player.x - 10) && (i < 10 + player.x || i > player.y - 10)*/) {
                         System.out.println(".");
                     } else {
                         System.out.println(map[i][j] + " ");
@@ -95,61 +90,61 @@ public class ArrayGame {
             System.out.println("Where do you want to move? (N/E/S/W/NE/NW/SE/SW) Or type Q to quit");
             direction = scan.next();
             if (direction.toUpperCase().contains("N")) {
-                map[x][y] = '.';
-                x -= 1;
+                map[player.x][player.y] = '.';
+                player.x -= 1;
             }
             if (direction.toUpperCase().contains("E")) {
-                map[x][y] = '.';
-                y += 1;
+                map[player.x][player.y] = '.';
+                player.y += 1;
             }
             if (direction.toUpperCase().contains("S")) {
-                map[x][y] = '.';
-                x += 1;
+                map[player.x][player.y] = '.';
+                player.x += 1;
             }
             if (direction.toUpperCase().contains("W")) {
-                map[x][y] = '.';
-                y -= 1;
+                map[player.x][player.y] = '.';
+                player.y -= 1;
             }
             if (direction.toUpperCase().contains("Q")) {
                 play = false;
             }
 
-            if (x > 19) {
-                x = 1;
-            } else if (x < 1) {
-                x = 19;
+            if (player.x > 19) {
+                player.x = 1;
+            } else if (player.x < 1) {
+                player.x = 19;
             }
-            if (y > 19) {
-                y = 1;
-            } else if (y < 1) {
-                y = 19;
+            if (player.y > 19) {
+                player.y = 1;
+            } else if (player.y < 1) {
+                player.y = 19;
             }
 
             map[ex][ey] = '.';
-            if (ex < x) {
+            if (ex < player.x) {
                 ex += 1;
-            } else if (ex > x) {
+            } else if (ex > player.x) {
                 ex -= 1;
             }
-            if (ey < y) {
+            if (ey < player.y) {
                 ey += 1;
-            } else if (ey > y) {
+            } else if (ey > player.y) {
                 ey -= 1;
             }
 
             map[ex1][ey1] = '.';
-            if (ex1 < x) {
+            if (ex1 < player.x) {
                 ex1 += 1;
-            } else if (ex1 > x) {
+            } else if (ex1 > player.x) {
                 ex1 -= 1;
             }
-            if (ey1 < y) {
+            if (ey1 < player.y) {
                 ey1 += 1;
-            } else if (ey1 > y) {
+            } else if (ey1 > player.y) {
                 ey1 -= 1;
             }
 
-            player = x + "," + y;
+            playercoords = player.x + "," + player.y;
             trap1 = trapx + "," + trapy;
             trap2 = trap1x + "," + trap1y;
             chest = cx + "," + cy;
@@ -163,16 +158,18 @@ public class ArrayGame {
             if (enemy1.equals(trap1) || enemy1.equals(trap2)) {
                 enemyAlive1 = false;
             }
-            if (player.equals(chest) && chestAlive) {
-                score += 10;
+            if (playercoords.equals(chest) && chestAlive) {
+                player.score += 10;
                 chestAlive = false;
             }
-            if (player.equals(chest1) && chestAlive1) {
-                score += 10;
+            if (playercoords.equals(chest1) && chestAlive1) {
+                player.score += 10;
                 chestAlive1 = false;
             }
-            System.out.println("Score = " + score);
-            play = checkIfOver(player, trap1, trap2, enemy, chest, enemyAlive, enemy1, enemyAlive1, chest1, score);
+            System.out.println("Score = " + player.score);
+            if (play) {
+            play = checkIfOver(playercoords, trap1, trap2, enemy, chest, enemyAlive, enemy1, enemyAlive1, chest1, player.score);
+            }
         }
     }
 
@@ -198,7 +195,7 @@ public class ArrayGame {
         }
     }
 
-    static boolean checkIfOver(String b /*player*/, String c/*trap1*/, String d/*trap2*/, String e/*enemy*/, String f/*chest*/, boolean g/*enemyAlive*/, String h/*enemy1*/, boolean i/*enemyAlive1*/, String j/*chest1*/, int k /*score*/) {
+    static boolean checkIfOver(String b /*playercoords*/, String c/*trap1*/, String d/*trap2*/, String e/*enemy*/, String f/*chest*/, boolean g/*enemyAlive*/, String h/*enemy1*/, boolean i/*enemyAlive1*/, String j/*chest1*/, int k /*score*/) {
         if (b.equals(c) || b.equals(d)) {
             System.out.println("You jumped into a trap.");
             System.out.println("__   __            _                   \n"
--- src/arraygame/Player.java
+++ src/arraygame/Player.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arraygame;
 
 /**
@@ -10,20 +5,22 @@ package arraygame;
  * @author Ramborux
  */
 public class Player {
+
     int x = 0;
 	String name;
-	int y = 0;
-	int z = 0;
+	int y;
+	int z;
 	int health = 100;
 	int score = 0;
-	String skill; 
-        int level = 1;
+	int level = 1;
 	char symbol;	
 
-	Player(String a, int b, int c, char d) {
+	Player(String a, int b, int c, char d, int e) {
 		this.x = b;
 		this.y = c;
 		this.name = a;
 		this.symbol = d;
+                this.score = e;
 	}
-}
+
+    }
